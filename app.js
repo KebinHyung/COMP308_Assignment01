91e7afa07d765786789e4e728af98e3ff237eee4
Kevin Luu <kevin_luu_naruto@hotmail.com>
2017-02-04 21:44:52 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+let express = require('express');
+let path = require('path'); //part of node.js core
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+
+let index = require('./routes/index');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//import "mongoose"
+let mongoose = require("mongoose");
+
+//URI
+//let URI = "mongodb://localhost/videogames";
+let URI = "mongodb://kevin:123456@ds031591.mlab.com:31591/videogames";
+mongoose.connect(URI,(err)=> {
+  if(err) {
+    console.log("Error connecting to MongoDB Server: " + err);
+  }
+  else {
+    console.log("Connected to MongoDB");
+  }
+})
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
